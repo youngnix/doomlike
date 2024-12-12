a58fd6f9cca45db969fb8e517a539bf2a9935a76
Andrei de Souza <71538656+youngnix@users.noreply.github.com>
2024-12-12 16:11:27 -0300

    feat: render basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
-#include <iostream>
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Window/Event.hpp"
+#include "SFML/Window/VideoMode.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/Main.hpp>
+#include <SFML/Window.hpp>
 
 int
 main(void)
 {
-    std::cout << "Hey, listen!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(640, 360), "doomlike");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::Closed:
+            {
+                window.close();
+                break;
+            }
+            default:
+                break;
+            }
+        }
+
+        window.clear();
+
+        window.display();
+    }
     return 0;
 }
