fca5448c8ebdfd9c53ccd7d36086f40dce60846f
Ronald Frota <65458407+Danzai-Senpai@users.noreply.github.com>
2024-12-13 16:01:03 -0300

    Explanatory comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,36 @@
-#include "SFML/Graphics/RenderWindow.hpp"
-#include "SFML/OpenGL.hpp"
-#include "SFML/Window/ContextSettings.hpp"
-#include "SFML/Window/Event.hpp"
-#include "SFML/Window/VideoMode.hpp"
-#include "SFML/Window/WindowStyle.hpp"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/OpenGL.hpp>
+#include <SFML/Window/ContextSettings.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/VideoMode.hpp>
+#include <SFML/Window/WindowStyle.hpp>
 #include "player.hpp"
 
-int
-main(void)
+int main(void)
 {
     // Configures the OpenGL context used by SFML
     sf::ContextSettings settings;
 
+    // Define the OpenGL 3.3
     settings.majorVersion = 3;
     settings.minorVersion = 3;
 
+    // sf::RenderWindow window() create a window called doonlike, set a size for this window, use the default style for this an apply the opengl settings
     sf::RenderWindow window(sf::VideoMode(640, 360), "doomlike", sf::Style::Default, settings);
 
+    // Initialize the player
     Player player;
 
+    // Get Delta Time
 	sf::Clock clock;
+
+    // Main Loop that have the responsibility to update the Delta Time, update the Player and render this in the window
     while (window.isOpen())
     {
     	float delta = clock.restart().asSeconds();
         sf::Event event;
 
+        // Window Events that for now we have just the close event
         while (window.pollEvent(event))
         {
             switch (event.type)
