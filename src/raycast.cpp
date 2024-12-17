c13771e0d8b1292e051ce54f06ea654a98482ed1
Ronald Frota <65458407+Danzai-Senpai@users.noreply.github.com>
2024-12-17 15:22:42 -0300

    feat: reestruct raycast

--- src/raycast.cpp
+++ src/raycast.cpp
@@ -1,4 +1,5 @@
-#include "SFML/Graphics/PrimitiveType.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/PrimitiveType.hpp>
 #include "tiles.hpp"
 #include "raycast.hpp"
 #include <cmath>
@@ -6,141 +7,101 @@
 #define screenWidth 640
 #define screenHeight 480
 
-Raycaster::Raycaster() {
-	this->planeX = 0;
-	this->planeY = 0.66;
+// Put a limit do DDA dont run a infinite distance
+const float MAX_RAY_LENGTH = 300.0f;
+
+// Defining colors for tiles
+const sf::Color tileColors[] = {
+    sf::Color::Transparent,       // TILES_EMPTY
+    sf::Color(199, 56, 30, 255),  // TILES_BRICKS
+    sf::Color(192, 112, 50, 255), // TILES_DIRT
+    sf::Color(255, 240, 74, 255), // TILES_LIGHT
+};
+
+// Constructors using Initialization List
+Raycaster::Raycaster() : planeX(0), planeY(0.66) {}
+Raycaster::Raycaster(float planeX, float planeY) : planeX(planeX), planeY(planeY) {}
+
+void calculateRayDirection(int x, float dirX, float dirY, float planeX, float planeY, double &rayDirX, double &rayDirY) {
+    double cameraX = 2 * x / double(screenWidth) - 1;
+    rayDirX = dirX + planeX * cameraX;
+    rayDirY = dirY + planeY * cameraX;
 }
 
-Raycaster::Raycaster(float planeX, float planeY) {
-	this->planeX = planeX;
-	this->planeY = planeY;
+bool performDDA(int &mapX, int &mapY, double rayDirX, double rayDirY, double &sideDistX, double &sideDistY, double deltaDistX, double deltaDistY, int &stepX, int &stepY, int &side, Tilemap &tilemap, TileType &hitTile) {
+    while (mapX >= 0 && mapY >= 0 && mapX < tilemap.width && mapY < tilemap.height) {
+        // Determine next position of ray using as a base which edge will be heat first
+		if (sideDistX < sideDistY) {
+            sideDistX += deltaDistX;
+            mapX += stepX;
+            side = 0;
+        } else {
+            sideDistY += deltaDistY;
+            mapY += stepY;
+            side = 1;
+        }
+
+        // Check collision with the tile
+        hitTile = tilemap.tiles[mapY * tilemap.width + mapX];
+        if (hitTile != TILES_EMPTY) {
+            return true;
+        }
+    }
+    return false;
 }
 
 void Raycaster::Cast(sf::RenderWindow &window, sf::Vector2f pos, float angle, Tilemap &tilemap) {
-	float dirX = std::sin(angle);
-	float dirY = -std::cos(angle);
+    float dirX = std::sin(angle);
+    float dirY = -std::cos(angle);
 
-	for (int x = 0; x < screenWidth; x++) {
-		// Calculate ray position and direction
-		double cameraX = 2 * x / double(screenWidth) - 1;
+    sf::VertexArray wallLines(sf::Lines);
 
-		double rayDirX = dirX + planeX * cameraX;
-		double rayDirY = dirY + planeY * cameraX;
-		
-		// Which box of the map we are
-		int mapX = pos.x;
-		int mapY = pos.y;
+    for (int x = 0; x < screenWidth; x++) {
+        // Calculate ray position and direction
+        double rayDirX, rayDirY;
+        calculateRayDirection(x, dirX, dirY, planeX, planeY, rayDirX, rayDirY);
 
-		// Length of ray from current position to next x or y-side
-		double sideDistX;
-		double sideDistY;
+        int mapX = int(pos.x);
+        int mapY = int(pos.y);
 
-		// Length of ray from one x or y-side to nex x or y-side
-		double deltaDistX = std::abs(1 / rayDirX);
-		double deltaDistY = std::abs(1 / rayDirY);
+        double deltaDistX = (rayDirX == 0) ? 1e30 : std::abs(1 / rayDirX);
+        double deltaDistY = (rayDirY == 0) ? 1e30 : std::abs(1 / rayDirY);
 
-		double perpWallDist;
+        double sideDistX = (rayDirX < 0) ? (pos.x - mapX) * deltaDistX : (mapX + 1.0 - pos.x) * deltaDistX;
+        double sideDistY = (rayDirY < 0) ? (pos.y - mapY) * deltaDistY : (mapY + 1.0 - pos.y) * deltaDistY;
 
-		int stepX, stepY;
-		int hit = 0;
-		int side;
+        int stepX = (rayDirX < 0) ? -1 : 1;
+        int stepY = (rayDirY < 0) ? -1 : 1;
 
-		// Calculate step and initial sideDist
-		if (rayDirX < 0) {
-			stepX = -1;
-			sideDistX = (pos.x - mapX) * deltaDistX;
-		} else {
-			stepX = 1;
-			sideDistX = (mapX + 1.0 - pos.x) * deltaDistX;
-		}
+        int side;
+        TileType hitTile = TILES_EMPTY;
 
-		if (rayDirY < 0) {
-			stepY = -1;
-			sideDistY = (pos.y - mapY) * deltaDistY;
-		} else {
-			stepY = 1;
-			sideDistY = (mapY + 1.0 - pos.y) * deltaDistY;
-		}
+        // Start DDA
+        bool hit = performDDA(mapX, mapY, rayDirX, rayDirY, sideDistX, sideDistY, deltaDistX, deltaDistY, stepX, stepY, side, tilemap, hitTile);
 
-		float maxLength = 300;
-		float length = 0;
-		TileType hitTile = TILES_EMPTY;
-		//
-		// Perform DDA
-		while (!hit
-			&& length < maxLength
-			&& mapX > 0
-			&& mapY > 0
-			&& mapX < tilemap.width
-			&& mapY < tilemap.height) {
-			//
-			// Jump to next map square, either in x-direction, or in y-direction
-			if (sideDistX < sideDistY) {
-				length += deltaDistX;
-				sideDistX += deltaDistX;
-				mapX += stepX;
-				side = 0;
-			} else {
-				length += deltaDistY;
-				sideDistY += deltaDistY;
-				mapY += stepY;
-				side = 1;
-			}
+        if (hit) {
+            // Calculate Wall Distance
+            double perpWallDist = (side == 0) ? (sideDistX - deltaDistX) : (sideDistY - deltaDistY);
 
-			// Check if ray has hit a wall
-			hitTile = tilemap.tiles[mapY * tilemap.width + mapX];
+            // Determine the Height of the Line that will be drawn
+            int lineHeight = std::min(int(screenHeight / perpWallDist), screenHeight);
 
-			if (hitTile != TILES_EMPTY) {
-				hit = 1;
-			}
-		}
+            int drawStart = std::max(-lineHeight / 2 + screenHeight / 2, 0);
+            int drawEnd = std::min(lineHeight / 2 + screenHeight / 2, screenHeight - 1);
 
-		if (hit) {
-			// Calculate distance projected on camera direction
-			if (side == 0) {
-				perpWallDist = (sideDistX - deltaDistX);
-			} else {
-				perpWallDist = (sideDistY - deltaDistY);
-			}
+            // Color and Shadow
+            sf::Color color = tileColors[hitTile];
+            if (side == 1) { // Escurecer para dar efeito 3D
+                color.r /= 1.5;
+                color.g /= 1.5;
+                color.b /= 1.5;
+            }
 
-			// Calculate height of line to draw on screen
-			int lineHeight = screenHeight / perpWallDist;
+            // Add lines to VertexArray
+            wallLines.append(sf::Vertex(sf::Vector2f(x, drawStart), color));
+            wallLines.append(sf::Vertex(sf::Vector2f(x, drawEnd), color));
+        }
+    }
 
-			// Calculate lowest and highest pixel to fill in current stripe
-			int drawStart = -lineHeight / 2 + screenHeight / 2;
-			if (drawStart < 0) {
-				drawStart = 0;
-			}
-
-			int drawEnd = lineHeight / 2 + screenHeight / 2;
-			if (drawEnd >= screenHeight) {
-				drawEnd = screenHeight - 1;
-			}
-
-			unsigned int color;
-			switch (hitTile) {
-				case TILES_BRICKS:
-					color = 0xc7381eFF;
-					break;
-				case TILES_DIRT:
-					color = 0xc07032FF;
-					break;
-				case TILES_LIGHT:
-					color = 0xfff04aff;
-					break;
-				default:
-					continue;
-			}
-
-			sf::Vertex line[2] = {
-				sf::Vertex(sf::Vector2f(x, drawStart)),
-				sf::Vertex(sf::Vector2f(x, drawEnd)),
-			};
-
-			line[0].color = sf::Color(color);
-			line[1].color = sf::Color(color);
-
-			window.draw(line, 2, sf::Lines);
-		}
-	}
+    window.draw(wallLines);
 }
